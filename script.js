bfe7a5e3a8591fce8a7b0bf75d8854cea9702801
VanyaMark <markova.vanya@gmail.com>
2023-11-28 22:27:25 +0100

    created function compareGuesses

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => {
-    return Math.floor(Math.random() *10);
-}
+  return Math.floor(Math.random() * 10);
+};
 
-
-
-
+const compareGuesses = (humanGuess, computerGuess, targetToGuess) => {
+  if (
+    Math.abs(targetToGuess - humanGuess) ===
+    Math.abs(targetToGuess - computerGuess)
+  ) {
+    return true;
+  } else if (
+    Math.abs(targetToGuess - humanGuess) <
+    Math.abs(targetToGuess - computerGuess)
+  ) {
+    return true;
+  } else if (
+    Math.abs(targetToGuess - humanGuess) >
+    Math.abs(targetToGuess - computerGuess)
+  ) {
+    return false;
+  }
+};
